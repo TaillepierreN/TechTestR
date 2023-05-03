ea591f35ffc4de3ec7f16d37db32c55ad8d88170
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-05-03 10:21:43 +0200

    Added SceneManager

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PreviousScene()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex-1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void NextScene()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex+1);
     }
 }
