4d51a4740a25f7ccb9d2cb77bec0e31358f72016
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-05-03 11:37:32 +0200

    added send survey function and screen

--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class QuestionManager : MonoBehaviour
 {
     [SerializeField] Toggle FirstAnswerToQuestion1;
     [SerializeField] Toggle FirstAnswerToQuestion2;
+    [SerializeField] TMP_InputField commentaryQuestion3;
+    [SerializeField] TMP_Text answer1;
+    [SerializeField] TMP_Text answer2;
+    [SerializeField] TMP_Text answer3;
+    [SerializeField] GameObject surveyPanel;
+    [SerializeField] GameObject sentPanel;
     string firstAnswer;
     string secondAnswer;
     string commentary;
@@ -20,8 +27,9 @@ public class QuestionManager : MonoBehaviour
     {
         
     }
-    void SendSurvey()
+    public void SendSurvey()
     {
+
         if(FirstAnswerToQuestion1.isOn)
         {
             firstAnswer = "Oui";
@@ -38,5 +46,12 @@ public class QuestionManager : MonoBehaviour
         {
             secondAnswer = "Non";
         }
+        commentary = commentaryQuestion3.text;
+        surveyPanel.SetActive(false);
+        sentPanel.SetActive(true);
+        answer1.text = firstAnswer;
+        answer2.text = secondAnswer;
+        answer3.text = commentary;
+
     }
 }
